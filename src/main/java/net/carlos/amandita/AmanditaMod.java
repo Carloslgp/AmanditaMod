74c8caedf261c9526baec8389892e82817131c4a
Carlos Leonardo <carloslgp3585@gmail.com>
2025-04-26 16:41:19 -0300

    Adding Amandita Gem in the game

--- src/main/java/net/carlos/amandita/AmanditaMod.java
+++ src/main/java/net/carlos/amandita/AmanditaMod.java
@@ -1,6 +1,8 @@
 package net.carlos.amandita;
 
 import com.mojang.logging.LogUtils;
+import net.carlos.amandita.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -37,6 +39,9 @@ public class AmanditaMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -52,7 +57,9 @@ public class AmanditaMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.Amandita);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
